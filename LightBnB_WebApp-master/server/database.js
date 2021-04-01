98e0ef2032396eeaa27a84a0c05eb31ae120af82
Penny Clark <respect.cactus@gmail.com>
2021-04-01 22:31:53 +0000

    Update addProperty function to add new listings to database

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -121,7 +121,7 @@ exports.getAllReservations = getAllReservations;
     queryString += `WHERE owner_id LIKE $${queryParams.length} `;
     }
   }
-console.log(options)
+
   if (options.minimum_price_per_night && options.maximum_price_per_night) {
     queryParams.push(options.minimum_price_per_night * 100);
     queryParams.push(options.maximum_price_per_night * 100);
@@ -161,9 +161,12 @@ exports.getAllProperties = getAllProperties;
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function(property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+  return pool.query(`INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms, country, street, city, province, post_code)
+  VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14) RETURNING *;`, [property.owner_id, property.title, property.description, property.thumbnail_photo_url, property.cover_photo_url, property.cost_per_night, property.parking_spaces, property.number_of_bathrooms, property.number_of_bedrooms, property.country, property.street, property.city, property.province, property.post_code])
+  .then((res) => {
+    const property = res.rows[0];
+    console.log(property);
+    return property;
+  })
 }
 exports.addProperty = addProperty;
